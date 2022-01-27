6a8327c86a8ab100a965c8400fec78dc3d68d077
Cinnamon <cinnamon_bun_github@fastmail.com>
2022-01-27 07:48:46 -0800

    deno fmt

--- signing/deno/index.ts
+++ signing/deno/index.ts
@@ -1,4 +1,4 @@
-import test from 'https://esm.sh/tape@5.4.0?target=deno';
+import test from "https://esm.sh/tape@5.4.0?target=deno";
 
 //import {
 //  assert,
@@ -10,14 +10,14 @@ import {
     AuthorKeypair,
     Crypto,
     CryptoDriverNoble,
-//    CryptoDriverTweetnacl,  // this was removed from the stone-soup esm-first branch?
+    //    CryptoDriverTweetnacl,  // this was removed from the stone-soup esm-first branch?
     GlobalCryptoDriver,
     ICryptoDriver,
-    ValidationError,
     isErr,
     notErr,
     setGlobalCryptoDriver,
-} from '../../../stone-soup/mod.ts';
+    ValidationError,
+} from "../../../stone-soup/mod.ts";
 //} from 'https://cdn.pika.dev/stone-soup@8.0.0';   // does not work
 //} from 'https://cdn.skypack.dev/stone-soup@8.0.0?dts';   // does not work
 //} from 'https://esm.sh/stone-soup@8.0.0?target=deno';   // does not work
@@ -25,20 +25,23 @@ import {
 const cryptoTests = (cryptoDriver: ICryptoDriver) => {
     const knownKeypair: AuthorKeypair = {
         "address": "@test.barbz37gfsbidaud5hq27x7opnu4p26axgvj737e7lsqjr5ozumxq",
-        "secret": "brkb5wo73qvs6q53yb4sxp4xxy6tmdlr3idqjalnqizshutndyeha"
-    }
-    const msg = 'hello';
-    const expectedSig = 'bkd372vxiqdu323dk65tn5s43zr6xpbkk2d7hoxj73pn7f7b4oiyndefybdzjrelnloioqoy2n2zh6gfymhy6drh6d42gwwr7fociqda'; 
+        "secret": "brkb5wo73qvs6q53yb4sxp4xxy6tmdlr3idqjalnqizshutndyeha",
+    };
+    const msg = "hello";
+    const expectedSig =
+        "bkd372vxiqdu323dk65tn5s43zr6xpbkk2d7hoxj73pn7f7b4oiyndefybdzjrelnloioqoy2n2zh6gfymhy6drh6d42gwwr7fociqda";
 
     const cryptoDriverName = (cryptoDriver as any).name;
 
     test(`${cryptoDriverName}: generateAuthorKeypair`, async (t) => {
         setGlobalCryptoDriver(cryptoDriver);
-        const newKeypair: AuthorKeypair | ValidationError  = await Crypto.generateAuthorKeypair('test');
-        t.equal(isErr(newKeypair), false, 'not an error');
+        const newKeypair: AuthorKeypair | ValidationError = await Crypto.generateAuthorKeypair(
+            "test",
+        );
+        t.equal(isErr(newKeypair), false, "not an error");
         if (notErr(newKeypair)) {
-            t.equal(typeof newKeypair.address, 'string', 'keypair has address');
-            t.equal(typeof newKeypair.secret, 'string', 'keypair has secret');
+            t.equal(typeof newKeypair.address, "string", "keypair has address");
+            t.equal(typeof newKeypair.secret, "string", "keypair has secret");
         }
         t.end();
     });
@@ -47,33 +50,37 @@ const cryptoTests = (cryptoDriver: ICryptoDriver) => {
         setGlobalCryptoDriver(cryptoDriver);
         const sig = await Crypto.sign(knownKeypair, msg);
         if (isErr(sig)) {
-            t.fail('sign() returned an error');
+            t.fail("sign() returned an error");
             return;
         }
-        t.equal(sig, expectedSig, 'sig should match expected sig');
+        t.equal(sig, expectedSig, "sig should match expected sig");
         t.end();
     });
 
     test(`${cryptoDriverName}: verify`, async (t) => {
         setGlobalCryptoDriver(cryptoDriver);
         const verifiedGood = await Crypto.verify(knownKeypair.address, expectedSig, msg);
-        const verifiedBad = await Crypto.verify(knownKeypair.address, expectedSig, 'some other message');
-        t.ok(verifiedGood, 'verified a good signature');
-        t.notOk(verifiedBad, 'rejected a bad signature');
+        const verifiedBad = await Crypto.verify(
+            knownKeypair.address,
+            expectedSig,
+            "some other message",
+        );
+        t.ok(verifiedGood, "verified a good signature");
+        t.notOk(verifiedBad, "rejected a bad signature");
         t.end();
     });
-}
+};
 
 const drivers = [
-    GlobalCryptoDriver,  // TODO: this is CryptoDriverNoble but let's test that the default value works
+    GlobalCryptoDriver, // TODO: this is CryptoDriverNoble but let's test that the default value works
     CryptoDriverNoble,
-//    CryptoDriverTweetnacl,
+    // CryptoDriverTweetnacl,
 ];
 for (const driver of drivers) {
     cryptoTests(driver);
 }
 
 test.onFailure(() => {
-    console.log('FAIL');
+    console.log("FAIL");
     Deno.exit(2);
 });
